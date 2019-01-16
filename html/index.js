f0243bd640dcb998685073559df748d9c94805f5
BobbaTea <bobba.akash@gmail.com>
2019-01-15 22:44:35 -0500

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,8 +1,6 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-$(document).ready(function () {
-    $(".dropdown-toggle").dropdown();
-});
+
 canvas.width = innerWidth
 canvas.height = innerHeight
 const maxRadius = 8
