2d915a97fdb122481db93d212630dd74fb5bda72
BobbaTea <bobba.akash@gmail.com>
2019-01-18 17:16:52 -0500

    testing

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+gulp.task('nunjucks', function() {
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('html/pages/**/*.+(html|nunjucks)')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['html/templates']
+      }))
+    // output files in app folder
+    .pipe(gulp.dest('html'))
+  });
